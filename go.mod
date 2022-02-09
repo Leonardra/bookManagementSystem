3c81b1346c29a4d6e77651056651c6c77625e8be
Oluwatobi Jolayemi <jolayemi.tobi@gmail.com>
2022-02-09 02:17:14 +0100

    Database setup

---
+++ go.mod
@@ -0,0 +1,11 @@
+module bookManagementSystem
+
+go 1.16
+
+require (
+	github.com/google/go-cmp v0.5.5 // indirect
+	github.com/joho/godotenv v1.4.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	go.mongodb.org/mongo-driver v1.8.3
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
---
+++ models/models.go
@@ -0,0 +1,25 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Book struct{
+	Id primitive.ObjectID `json:"id" bson:"id"`
+	Author primitive.ObjectID `json:"author_id" bson:"author_id"`
+	Title string `json:"title" bson:"title"`
+	Genre string `json:"genre" bson:"genre"`
+	BookDescription string `json:"book_description omitempty" bson:"book_description omitempty"`
+	Tags []string `json:"tags" bson:"tags"`
+	Quantity int  `json:"quantity" bson:"quantity"`
+	ReleaseDate time.Time `json:"release_date" bson:"release_date"`
+}
+
+type Author struct{
+	Id primitive.ObjectID  `json:"id" bson:"id"`
+	FirstName string `json:"firstName" bson:"firstName"`
+	LastName string `json:"lastName" bson:"lastName"`
+	EmailAddress string `json:"emailAddress" bson:"emailAddress"`
+	Books []Book
+}
